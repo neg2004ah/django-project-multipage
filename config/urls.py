1a91a6068533ef4b5d4260f3ad3a132bfdda1643
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-04-20 17:03:24 +0430

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT) 
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT) 
