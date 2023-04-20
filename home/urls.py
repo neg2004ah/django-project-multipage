5fb86a722fda18e875c4b07545eac4a8479e5b97
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-04-20 18:32:15 +0430

    template tags
    inclution tag ,block tag , extends tag

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,9 @@ from .views import *
 
 urlpatterns = [
     path('',home),
+    path('about',about),
+    path('blog-single',blog_single),
+    path('blog-home',blog_home),
+    path('contact',contact),
+    path('elements',elements),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home (request):
     return render(request,'home/index.html')
+
+def blog_home (request):
+    return render(request,'home/blog-home.html')
+
+def blog_single (request):
+    return render(request,'home/blog-single.html')
+
+def contact (request):
+    return render(request,'home/contact.html')
+
+def elements (request):
+    return render(request,'home/elements.html')
+
+def about (request):
+    return render(request,'home/about.html')
