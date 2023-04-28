7429bf1abc0cdb5a827bc5af411cbbadaa4e72ae
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-04-28 19:02:01 +0430

    database
    for manage database and make default django table

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,14 @@
 from django.urls import *
 from .views import *
 
+app_name = 'home'
 
 
 urlpatterns = [
-    path('',home),
-    path('about',about),
-    path('blog-single',blog_single),
-    path('blog-home',blog_home),
-    path('contact',contact),
-    path('elements',elements),
+    path('',home,name = 'home'),
+    path('about',about,name ='about'),
+    path('blog-single',blog_single,name = 'blog_single'),
+    path('blog-home',blog_home,name = 'blog_home'),
+    path('contact',contact,name = 'contact'),
+    path('elements',elements,name = 'elements'),
 ]
