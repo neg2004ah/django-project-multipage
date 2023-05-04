0186c5db0401f2f49c287302a9e9cda97f8706b5
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-05-05 04:23:42 +0430

    SOME CHANGE

--- config/setting/development.py
+++ config/setting/development.py
@@ -42,8 +42,8 @@ DATABASES = {
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR.joinpath('/static')
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR.joinpath('/media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',
